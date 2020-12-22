1290b8b6e03f923cd1b7648d5b3e429c8c00a73d
Direct-eks <35151167+Direct-eks@users.noreply.github.com>
2020-12-22 14:25:53 -0500

    Update ModelServiceImpl.java

--- backend/src/main/java/org/jc/backend/service/Impl/ModelServiceImpl.java
+++ backend/src/main/java/org/jc/backend/service/Impl/ModelServiceImpl.java
@@ -7,8 +7,6 @@ import org.jc.backend.service.ModelService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -25,12 +23,10 @@ public class ModelServiceImpl implements ModelService {
 
     /* ------------------------------ SERVICE ------------------------------ */
 
-    @GetMapping()
     public List<ModelCategoryO> getModelCategories() {
         return modelMapper.queryModelCategories();
     }
 
-    @PostMapping()
     public List<ModelO> getModelsByCategory(int id) {
         return modelMapper.queryModelsByCategory(id);
     }
