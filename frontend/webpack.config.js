43852411d152b98044db6221dbabd74a12e846f5
Direct-eks <35151167+Direct-eks@users.noreply.github.com>
2020-12-21 19:39:24 -0500

    frontend init

---
+++ frontend/webpack.config.js
@@ -0,0 +1,112 @@
+const resolve = require('path').resolve
+const webpack = require('webpack')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const url = require('url')
+const publicPath = ''
+
+const bundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+
+module.exports = (options = {}) => ({
+    entry: {
+        // vendor: './src/vendor',
+        main: './src/main.js'
+    },
+    output: {
+        path: resolve(__dirname, 'dist'),
+        filename: options.dev ? '[name].js' : '[name].js?[chunkhash]',
+        chunkFilename: '[id].js?[chunkhash]',
+        publicPath: options.dev ? '/assets/' : publicPath
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                use: ['vue-loader']
+            },
+            {
+                test: /\.js$/,
+                // use: ['babel-loader'],
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            // 'babelrc': false,
+                            "plugins": [
+                                "@babel/plugin-syntax-dynamic-import"
+                            ]
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.s(c|a)ss$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader',
+                    {
+                        loader: 'sass-loader',
+                        // Requires sass-loader@^8.0.0
+                        options: {
+                            implementation: require('sass'),
+                            sassOptions: {
+                                fiber: require('fibers'),
+                                indentedSyntax: true // optional
+                            },
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.css$/,
+                use: ['vue-style-loader', 'css-loader', 'postcss-loader']
+            },
+            // {
+            //     test: /\.(png|jpg|jpeg|gif|eot|ttf|woff|woff2|svg|svgz)(\?.+)?$/,
+            //     use: [{
+            //         loader: 'url-loader',
+            //         options: {
+            //             limit: 10000
+            //         }
+            //     }]
+            // }
+        ]
+    },
+    resolve: {
+        alias: {
+            '~': resolve(__dirname, 'src')
+        },
+        extensions: ['.js', '.vue', '.json', '.css']
+    },
+    devtool: options.dev ? 'inline-source-map' : 'source-map',
+    devServer: {
+        host: '127.0.0.1',
+        port: 8080,
+        proxy: {
+            '/api': {
+                target: 'http://localhost:3000',
+                changeOrigin: true,
+                pathRewrite: { '^/api': '' }
+            }
+        },
+        historyApiFallback: {
+            index: url.parse(options.dev ? '/assets/' : publicPath).pathname
+        },
+        hot: true
+    },
+    plugins: [
+        new VueLoaderPlugin(),
+        new HtmlWebpackPlugin({
+            template: 'src/assets/index.html'
+        }),
+        new bundleAnalyzerPlugin(),
+        new VuetifyLoaderPlugin()
+    ],
+    optimization: {
+        splitChunks: {
+            chunks: 'async'
+        }
+    }
+})
