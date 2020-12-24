6410beda574cc43d23e670f63e33cf5cc51477a5
Direct-eks <35151167+Direct-eks@users.noreply.github.com>
2020-12-24 13:28:42 -0500

    update validation

--- backend/src/main/java/org/jc/backend/entity/VO/PurchaseOrderEntryWithProductsVO.java
+++ backend/src/main/java/org/jc/backend/entity/VO/PurchaseOrderEntryWithProductsVO.java
@@ -6,6 +6,7 @@ import org.jc.backend.entity.PurchaseOrderProductO;
 
 import javax.validation.Valid;
 import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import java.util.List;
@@ -15,8 +16,10 @@ import java.util.List;
 public class PurchaseOrderEntryWithProductsVO {
     private String purchaseOrderEntryID;
     @NotNull(message = "entryDate null error")
+    @NotBlank(message = "entryDate blank error")
     private String entryDate;
     @NotNull(message = "creationDate null error")
+    @NotBlank(message = "creationDate blank error")
     private String creationDate;
     @DecimalMin(value = "0.0", message = "totalCost smaller than zero error")
     private double totalCost;
@@ -24,6 +27,8 @@ public class PurchaseOrderEntryWithProductsVO {
     private String invoiceType;
     @Pattern(regexp = "^(执行|中止)$", message = "executionStatus value error")
     private String executionStatus;
+    @NotNull(message = "drawer null error")
+    @NotBlank(message = "drawer blank error")
     private String drawer;
 
     private int partnerCompanyID;
