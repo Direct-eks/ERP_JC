5886859a961c1b3f264ca9555bfae396379c82e8
Direct-eks <35151167+Direct-eks@users.noreply.github.com>
2020-12-28 16:35:08 -0500

    code clean

--- frontend/src/views/Inbound_Invoicing/3_Payment/4_Incomplete_Pay_Detail.vue
+++ frontend/src/views/Inbound_Invoicing/3_Payment/4_Incomplete_Pay_Detail.vue
@@ -2,51 +2,6 @@
     <!--  <p>入库结账管理</p>-->
     <!--  <p>入库未结账明细</p>-->
     <div id="all">
-        <el-row>
-            <strong>入库未结账明细</strong>
-        </el-row>
-
-        <el-form>
-            <el-form-item>
-                <label style="margin-right: 5px">供货单位全称</label>
-                <el-input style="width: 300px"
-                          placeholder="全称"
-                          v-model="fullSearchField">
-                </el-input>
-
-                <el-button @click="fullSearch()" :loading="fullSearchLoading" type="info"
-                           :disabled="fullSearchPanelOpen === true">
-                    单位助选
-                </el-button>
-
-                <el-button type="info">查询</el-button>
-                <el-button type="info">导出</el-button>
-            </el-form-item>
-
-            <el-collapse-transition>
-                <company-search v-if="fullSearchPanelOpen"
-                                @fullSearchPanelClose="fullSearchPanelCloseAction"
-                                @fullSearchChoose="fullSearchChooseAction">
-                </company-search>
-            </el-collapse-transition>
-        </el-form>
-
-        <el-table
-                :data="tableData"
-                stripe
-                highlight-current-row
-                style="width: 100%"
-                height="300">
-            <el-table-column fixed prop="date" label="序号" width="150"></el-table-column>
-            <el-table-column prop="name" label="新代号" width="120"></el-table-column>
-            <el-table-column prop="province" label="旧代号" width="120"></el-table-column>
-            <el-table-column prop="city" label="厂牌" width="120"></el-table-column>
-            <el-table-column prop="address" label="入库数量" width="300"></el-table-column>
-            <el-table-column prop="zip" label="单位" width="120"></el-table-column>
-            <el-table-column prop="zip" label="含税单价" width="120"></el-table-column>
-            <el-table-column prop="zip" label="不含税单价" width="120"></el-table-column>
-            <el-table-column prop="zip" label="不含税金额" width="120"></el-table-column>
-        </el-table>
 
     </div>
 </template>
@@ -92,13 +47,5 @@
 </script>
 
 <style scoped>
-    #all {
-        text-align: left;
-        padding: 10px;
-        box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04)
-    }
 
-    .el-row {
-        margin-bottom: 20px;
-    }
 </style>
--- frontend/src/views/Inbound_Invoicing/3_Payment/6_Incomplete_Invoice_Query.vue
+++ frontend/src/views/Inbound_Invoicing/3_Payment/6_Incomplete_Invoice_Query.vue
@@ -3,33 +3,7 @@
     <!--  <p>入库结账未开票查询</p>-->
     <div style="text-align: left">
 
-        <strong>入库结账未开票查询</strong>
 
-        <el-tabs type="border-card">
-            <el-tab-pane label="入库结账未开票按单位汇总查询">
-                <el-row>
-                    <el-button type="info">导出汇总</el-button>
-                    <el-button type="info">导出全部未开票</el-button>
-                    <el-button type="info">查询</el-button>
-                </el-row>
-
-                <el-table
-                        :data="tableData"
-                        stripe
-                        highlight-current-row
-                        style="width: 100%"
-                        height="300">
-                    <el-table-column fixed prop="date" label="序号" width="150"></el-table-column>
-                    <el-table-column prop="name" label="单位简称" width="120"></el-table-column>
-                    <el-table-column prop="province" label="单位全称" width="120"></el-table-column>
-                    <el-table-column prop="city" label="未开票金额" width="120"></el-table-column>
-                </el-table>
-            </el-tab-pane>
-
-            <el-tab-pane label="入库结账未开票按单位明细查询">
-
-            </el-tab-pane>
-        </el-tabs>
     </div>
 </template>
 
