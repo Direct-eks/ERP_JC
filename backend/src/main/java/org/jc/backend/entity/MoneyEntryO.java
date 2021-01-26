45b6851bd4636c36d843832bf543c9f72ed1616a
Direct-eks <35151167+Direct-eks@users.noreply.github.com>
2021-01-26 01:16:38 -0500

    Update MoneyEntryO.java

--- backend/src/main/java/org/jc/backend/entity/MoneyEntryO.java
+++ backend/src/main/java/org/jc/backend/entity/MoneyEntryO.java
@@ -2,6 +2,7 @@ package org.jc.backend.entity;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
@@ -9,10 +10,12 @@ import javax.validation.constraints.Pattern;
 
 @Getter
 @Setter
+@ToString
 public class MoneyEntryO {
     private String moneyEntrySerial;
 
-    private int partnerCompanyID;
+    @NotNull(message = "partnerCompanyID null error")
+    private Integer partnerCompanyID;
     //from c_partner_company
     private String companyAbbreviatedName;
     private String companyFullName;
@@ -22,7 +25,11 @@ public class MoneyEntryO {
     @Pattern(regexp = "^(正常|退款)$", message = "paymentIndication value error")
     private String paymentIndication;
 
+    @NotNull(message = "paymentMethod null error")
+    @Pattern(regexp = "^(现金|转支|电汇|汇票|其他)$", message = "paymentMethod value error")
     private String paymentMethod;
+
+    @NotNull(message = "paymentNumber null error")
     private String paymentNumber;
 
     @NotNull(message = "paymentAmount null error")
@@ -30,7 +37,8 @@ public class MoneyEntryO {
     @Pattern(regexp = "^[\\d]*?\\.?[\\d]*?$", message = "paymentAmount value error")
     private String paymentAmount;
 
-    private int bankAccountID;
+    @NotNull(message = "bankAccountID null error")
+    private Integer bankAccountID;
     // from c_bank_account
     private String bankAccountName;
 
@@ -41,11 +49,18 @@ public class MoneyEntryO {
     @NotBlank(message = "drawer blank error")
     private String drawer;
 
+    @NotNull(message = "creationDate null error")
+    @NotBlank(message = "creationDate blank error")
     private String creationDate;
+
+    @NotNull(message = "paymentDate null error")
+    @NotBlank(message = "paymentDate blank error")
     private String paymentDate;
+
     private String checkoutSerial;
 
-    private int departmentID;
+    @NotNull(message = "departmentID null error")
+    private Integer departmentID;
     // from e_department
     private String departmentName;
 
