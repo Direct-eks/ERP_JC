1f16152175062a278332afad69a71abd1a1be85a
Direct-eks <35151167+Direct-eks@users.noreply.github.com>
2021-01-26 00:23:01 -0500

    Update OutboundEntryServiceImpl.java

--- backend/src/main/java/org/jc/backend/service/Impl/OutboundEntryServiceImpl.java
+++ backend/src/main/java/org/jc/backend/service/Impl/OutboundEntryServiceImpl.java
@@ -21,7 +21,6 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 
 import java.math.BigDecimal;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
