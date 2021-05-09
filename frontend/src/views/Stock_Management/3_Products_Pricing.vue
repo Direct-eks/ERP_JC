253ae7653de0a8464dc203a240b44aa07b27375b
Direct-eks <35151167+Direct-eks@users.noreply.github.com>
2021-05-09 19:35:32 -0400

    Update 3_Products_Pricing.vue

--- frontend/src/views/Stock_Management/3_Products_Pricing.vue
+++ frontend/src/views/Stock_Management/3_Products_Pricing.vue
@@ -133,6 +133,33 @@
 
         </div>
 
+        <v-data-table
+                      :headers="tableHeaders"
+                      :items="tableData"
+                      item-key="index"
+                      height="45vh"
+                      calculate-widths
+                      disable-sort
+                      show-select
+                      fixed-header
+                      disable-pagination
+                      hide-default-footer
+                      locale="zh-cn">
+            <template v-slot:item.index="{ item }">
+                {{tableData.indexOf(item) + 1}}
+            </template>
+            <template v-slot:item.storagePlace="{ item }">
+                <v-select v-model="item.storagePlace"
+                          :items="storagePlaceOptions"
+                          hide-details="auto"
+                          multiple
+                          @change="saveChoice(item)"
+                          dense
+                          style="width: 150px">
+                </v-select>
+            </template>
+        </v-data-table>
+
     </v-card>
 </template>
 
@@ -220,6 +247,19 @@ export default {
             ],
             supplierTableData: [],
             supplierTableCurrentRow: [],
+
+            storagePlaceOptions: ['a', 'b', 'c', 'd', 'e'],
+            storagePlaceChoose: [],
+            tableHeaders: [
+                { text: '序号', value: 'index', width: '65px' },
+                { text: '架位', value: 'storagePlace', width: '65px' },
+                { text: '选择', value: 'choice', width: '65px' },
+            ],
+            tableData: [
+                {storagePlace: [], choice: ''},
+                {storagePlace: [], choice: ''},
+                {storagePlace: [], choice: ''}
+            ],
         }
     },
     methods: {
@@ -252,6 +292,16 @@ export default {
             this.treeSelectedLevel = [{name: ''}]
             this.factoryBrandCurrentRow = [{code: ''}]
             this.warehouseTableCurrentRow = [{name: ''}]
+        },
+
+        //temp test method
+        saveChoice(item) {
+            console.log(item.storagePlace)
+            item.choice = item.storagePlace.toString()
+        },
+
+        searchSku() {
+
         }
     }
 }
