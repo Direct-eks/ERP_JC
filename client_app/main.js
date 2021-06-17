742c599ba34e1cca484a4dea8473910e68e2b043
xianqi wang <35151167+Direct-eks@users.noreply.github.com>
2021-06-17 17:08:59 +0800

    client app init

---
+++ client_app/main.js
@@ -0,0 +1,47 @@
+const { app, BrowserWindow, dialog } = require('electron')
+
+/* ---------------------- */
+let win
+
+app.whenReady().then(() => {
+    win = new BrowserWindow({
+        width: 600,
+        height: 400,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        },
+        useContentSize: true
+    })
+    win.on('close', (e) => {
+        // e.preventDefault();
+        let result = dialog.showMessageBoxSync({
+            type: "warning",
+            title: '退出确认',
+            message: '确认退出？',
+            buttons: ['取消', '确认']
+        })
+        if (result === 0) {
+            e.preventDefault()
+        } else {
+            return null
+        }
+    })
+    win.loadFile(__dirname + '/webpages/index.html', {hash: '#/Login'})
+        .then(() => win.maximize())
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        // createWindow()
+    }
+})
+
+
+/* ---------------------- */
