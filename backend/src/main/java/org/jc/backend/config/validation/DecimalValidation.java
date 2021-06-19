13e4eb57eed9e7caa04d3e1d78cb0efdf3b035f7
xianqi wang <35151167+Direct-eks@users.noreply.github.com>
2021-06-19 14:22:13 +0800

    fix custom annotation

--- backend/src/main/java/org/jc/backend/config/validation/DecimalValidation.java
+++ backend/src/main/java/org/jc/backend/config/validation/DecimalValidation.java
@@ -1,6 +1,7 @@
 package org.jc.backend.config.validation;
 
 import javax.validation.Constraint;
+import javax.validation.Payload;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -18,4 +19,6 @@ public @interface DecimalValidation {
     ValidationTypeEnum type();
     String message();
 
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
 }
