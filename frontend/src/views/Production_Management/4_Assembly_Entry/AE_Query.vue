f03bc52f8d11e5d6864cfb0f0da5ce5c0ccca8d1
xianqi wang <35151167+Direct-eks@users.noreply.github.com>
2021-07-18 19:23:27 +0800

    page init

--- frontend/src/views/Production_Management/4_Assembly_Entry/AE_Query.vue
+++ frontend/src/views/Production_Management/4_Assembly_Entry/AE_Query.vue
@@ -2,18 +2,43 @@
     <!--  <p>生产管理</p>-->
     <!--  <p>拆组装单查询</p>-->
     <v-card>
-        <v-card-title class="d-flex">
-            拆组装单查询
+        <v-toolbar flat>
+            <v-toolbar-title>拆组装单查询</v-toolbar-title>
             <v-spacer></v-spacer>
             <v-btn color="accent"
                    to="/production_management">
                 <v-icon>{{ mdiArrowLeft }}</v-icon>
                 返回
             </v-btn>
-        </v-card-title>
-        <v-card-text>
 
-        </v-card-text>
+            <template v-slot:extension>
+                <v-tabs v-model="tab" @change="handleTabChange">
+                    <v-tabs-slider></v-tabs-slider>
+                    <v-tab key="browse">浏览</v-tab>
+                    <v-tab key="detail" :disabled="currentTableRow === null">详细情况</v-tab>
+                </v-tabs>
+            </template>
+        </v-toolbar>
+
+        <v-tabs-items v-model="tab">
+
+            <v-tab-item key="browse">
+                <QueryComponent displayMode="display"
+                                type="assemblyEntry"
+                                prefix="拆装"
+                                @tableClick="tableClickAction">
+                </QueryComponent>
+            </v-tab-item>
+
+            <v-tab-item key="detail" :eager="true">
+                <EntryComponent editMode="display"
+                                type="assemblyEntry"
+                                prefix="拆" :paramFormsArray="forms">
+                </EntryComponent>
+            </v-tab-item>
+
+        </v-tabs-items>
+
     </v-card>
 </template>
 
@@ -22,9 +47,47 @@ import {mdiArrowLeft} from "@mdi/js";
 
 export default {
     name: "AE_Query",
+    components: {
+        EntryComponent: () => import('~/components/ProductionComponents/DuelEntry'),
+        QueryComponent: () => import('~/components/ProductionComponents/QueryDisplayComponent')
+    },
+    beforeMount() {
+        this.originalForms = JSON.parse(JSON.stringify(this.forms[0]))
+    },
     data() {
         return {
-            mdiArrowLeft
+            mdiArrowLeft,
+            tab: null,
+            currentTableRow: null,
+
+            forms: [{
+                entryDate: '', creationDate: '', totalAmount: '', drawer: '',
+                departmentID: -1, departmentName: '',
+                warehouseID: -1, warehouseName: '',
+                remark: '', classification: '',
+                entryProducts: []
+            }, {
+                entryDate: '', creationDate: '', totalAmount: '', drawer: '',
+                departmentID: -1, departmentName: '',
+                warehouseID: -1, warehouseName: '',
+                remark: '', classification: '',
+                entryProducts: []
+            }],
+            originalForms: {}
+        }
+    },
+    methods: {
+        handleTabChange(val) {
+            if (val === 0) {
+                this.currentTableRow = null
+                Object.assign(this.forms[0], this.originalForms) // reset form
+                Object.assign(this.forms[1], this.originalForms) // reset form
+            }
+        },
+        tableClickAction(val) {
+            this.currentTableRow = val
+            Object.assign(this.forms[0], this.currentTableRow[0])
+            Object.assign(this.forms[1], this.currentTableRow[1])
         }
     }
 }
--- frontend/src/views/Production_Management/7_Transfer/T_Modify.vue
+++ frontend/src/views/Production_Management/7_Transfer/T_Modify.vue
@@ -2,18 +2,43 @@
     <!--  <p>生产管理</p>-->
     <!--  <p>调拨单修改</p>-->
     <v-card>
-        <v-card-title class="d-flex">
-            调拨单修改
+        <v-toolbar flat>
+            <v-toolbar-title>调拨单修改</v-toolbar-title>
             <v-spacer></v-spacer>
             <v-btn color="accent"
                    to="/production_management">
                 <v-icon>{{ mdiArrowLeft }}</v-icon>
                 返回
             </v-btn>
-        </v-card-title>
-        <v-card-text>
 
-        </v-card-text>
+            <template v-slot:extension>
+                <v-tabs v-model="tab" @change="handleTabChange">
+                    <v-tabs-slider></v-tabs-slider>
+                    <v-tab key="browse">浏览</v-tab>
+                    <v-tab key="detail" :disabled="currentTableRow === null">详细情况</v-tab>
+                </v-tabs>
+            </template>
+        </v-toolbar>
+
+        <v-tabs-items v-model="tab">
+
+            <v-tab-item key="browse">
+                <QueryComponent displayMode="modification"
+                                type="transferEntry"
+                                prefix="调拨"
+                                @tableClick="tableClickAction">
+                </QueryComponent>
+            </v-tab-item>
+
+            <v-tab-item key="detail" :eager="true">
+                <EntryComponent editMode="modification"
+                                type="transferEntry"
+                                prefix="调" :paramFormsArray="forms">
+                </EntryComponent>
+            </v-tab-item>
+
+        </v-tabs-items>
+
     </v-card>
 </template>
 
@@ -22,9 +47,47 @@ import {mdiArrowLeft} from "@mdi/js";
 
 export default {
     name: "D_Modify",
+    components: {
+        EntryComponent: () => import('~/components/ProductionComponents/DuelEntry'),
+        QueryComponent: () => import('~/components/ProductionComponents/QueryDisplayComponent')
+    },
+    beforeMount() {
+        this.originalForms = JSON.parse(JSON.stringify(this.forms[0]))
+    },
     data() {
         return {
-            mdiArrowLeft
+            mdiArrowLeft,
+            tab: null,
+            currentTableRow: null,
+
+            forms: [{
+                entryDate: '', creationDate: '', totalAmount: '', drawer: '',
+                departmentID: -1, departmentName: '',
+                warehouseID: -1, warehouseName: '',
+                remark: '', classification: '',
+                entryProducts: []
+            }, {
+                entryDate: '', creationDate: '', totalAmount: '', drawer: '',
+                departmentID: -1, departmentName: '',
+                warehouseID: -1, warehouseName: '',
+                remark: '', classification: '',
+                entryProducts: []
+            }],
+            originalForms: {}
+        }
+    },
+    methods: {
+        handleTabChange(val) {
+            if (val === 0) {
+                this.currentTableRow = null
+                Object.assign(this.forms[0], this.originalForms) // reset form
+                Object.assign(this.forms[1], this.originalForms) // reset form
+            }
+        },
+        tableClickAction(val) {
+            this.currentTableRow = val
+            Object.assign(this.forms[0], this.currentTableRow[0])
+            Object.assign(this.forms[1], this.currentTableRow[1])
         }
     }
 }
