080fb9279d9232adbb364c12ef6443f7a5354112
xianqi wang <35151167+Direct-eks@users.noreply.github.com>
2021-07-20 21:00:24 +0800

    Update 5_Checkout_Return.vue

--- frontend/src/views/Inbound_Invoicing/1_Checkout/5_Checkout_Return.vue
+++ frontend/src/views/Inbound_Invoicing/1_Checkout/5_Checkout_Return.vue
@@ -2,18 +2,44 @@
     <!--  <p>入库结账管理</p>-->
     <!--  <p>入库结账后退货</p>-->
     <v-card>
-        <v-card-title class="d-flex">
-            入库结账后退货
+        <v-toolbar flat>
+            <v-toolbar-title>入库结账后退货</v-toolbar-title>
             <v-spacer></v-spacer>
             <v-btn color="accent"
                    to="/inbound_invoicing">
                 <v-icon>{{ mdiArrowLeft }}</v-icon>
                 返回
             </v-btn>
-        </v-card-title>
-        <v-card-text>
 
-        </v-card-text>
+            <template v-slot:extension>
+                <v-tabs v-model="tab" @change="handleTabChange">
+                    <v-tabs-slider></v-tabs-slider>
+                    <v-tab key="browse">浏览</v-tab>
+                    <v-tab key="detail" :disabled="currentTableRow === null">详细情况</v-tab>
+                </v-tabs>
+            </template>
+        </v-toolbar>
+
+        <v-tabs-items v-model="tab">
+
+            <v-tab-item key="browse">
+                <QueryDisplayComponent
+                    displayMode="return"
+                    :isInbound="true"
+                    @tableClick="tableClickAction">
+                </QueryDisplayComponent>
+            </v-tab-item>
+
+            <v-tab-item key="detail" :eager="true">
+                <CheckoutComponent
+                    :paramForm="form"
+                    mode="return"
+                    :isInbound="true">
+                </CheckoutComponent>
+            </v-tab-item>
+
+        </v-tabs-items>
+
     </v-card>
 </template>
 
@@ -23,15 +49,65 @@ import {mdiArrowLeft} from "@mdi/js";
 export default {
     name: "In_Check_Inv_Return",
     components: {
-
+        QueryDisplayComponent: () => import('~/components/InvoiceComponents/CheckoutQueryDisplayComponent'),
+        CheckoutComponent: () => import('~/components/InvoiceComponents/CheckoutComponent'),
     },
     data() {
         return {
-            mdiArrowLeft
+            mdiArrowLeft,
+            tab: null,
+            currentTableRow: null,
+
+            form: {
+                checkoutEntrySerial: '',
+                partnerCompanyID: -1,
+                companyAbbreviatedName: '', companyPhone: '',
+                companyFullName: '', companyRemark: '',
+                invoiceType: '',
+                paymentMethod: '', paymentNumber: '', paymentAmount: '',
+                bankAccountID: -1, bankAccountName: '',
+                totalAmount: '', isRounded: 0, roundedAmount: '',
+                debt: '', serviceFee: '',
+                remark: '', drawer: '',
+                creationDate: '',
+                checkoutDate: '',
+                moneyEntrySerial: '',
+                returnSerial: '',
+                departmentID: -1, departmentName: '',
+                isVerified: 0,
+                isModified: 0,
+                inboundCheckoutProducts: [],
+                outboundCheckoutProducts: [],
+                invoiceEntry: null
+            },
+            originalForm: {}
         }
     },
     methods: {
-
+        handleTabChange(val) {
+            if (val === 0) {
+                this.currentTableRow = null
+                this.form = Object.assign(this.form, this.originalForm)
+            }
+        },
+        tableClickAction(val) {
+            this.currentTableRow = val
+            this.form = Object.assign(this.form, this.currentTableRow)
+            this.form.paymentMethod = ''
+            this.form.paymentNumber = ''
+            this.form.paymentAmount = '0'
+            this.form.bankAccountID = -1
+            this.form.bankAccountName = ''
+            this.form.totalAmount = '0'
+            this.form.isRounded = 0
+            this.form.roundedAmount = '0'
+            this.form.debt = '0'
+            this.form.serviceFee = '0'
+            this.form.remark = ''
+            this.form.departmentID = -1
+            this.form.departmentName = ''
+            this.form.invoiceEntry = null
+        }
     }
 }
 </script>
