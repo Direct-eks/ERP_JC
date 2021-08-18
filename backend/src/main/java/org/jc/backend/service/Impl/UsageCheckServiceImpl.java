a10edc16e2c7b4f4d6ebe531e4d1fa4ab689d063
Direct-eks <35151167+Direct-eks@users.noreply.github.com>
2021-08-17 23:32:51 -0400

    Update UsageCheckServiceImpl.java

--- backend/src/main/java/org/jc/backend/service/Impl/UsageCheckServiceImpl.java
+++ backend/src/main/java/org/jc/backend/service/Impl/UsageCheckServiceImpl.java
@@ -155,6 +155,7 @@ public class UsageCheckServiceImpl implements UsageCheckService {
 
     @Override
     public boolean isFeeCategoryIDInUse(int categoryID) {
+        // todo, finish mapper sql
         return usageCheckMapper.findFeeCategoryIDInEntries(categoryID) != null;
     }
 }
