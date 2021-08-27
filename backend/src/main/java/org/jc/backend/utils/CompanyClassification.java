52691dfeeed349297c9838c706902fc47323087e
Direct-eks <35151167+Direct-eks@users.noreply.github.com>
2021-08-26 23:23:25 -0400

    fix inverted company classification

--- backend/src/main/java/org/jc/backend/utils/CompanyClassification.java
+++ backend/src/main/java/org/jc/backend/utils/CompanyClassification.java
@@ -1,8 +1,8 @@
 package org.jc.backend.utils;
 
 public class CompanyClassification {
-    public final static String SUPPLIER ="客户";
-    public final static String CUSTOMER ="供应商";
+    public final static String SUPPLIER ="供应商";
+    public final static String CUSTOMER ="客户";
     public final static String OTHER_RECV ="其它应收";
     public final static String OTHER_PAY ="其它应付";
 }
